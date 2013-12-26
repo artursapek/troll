1731513aa1b56091262ab468e0ffac9e1348454d
Artur Sapek <artur@codecademy.com>
2013-12-26 09:35:55 -0500

    Add free to simulation

--- src/troll/test_trading.go
+++ src/troll/test_trading.go
@@ -10,6 +10,8 @@ func (troll Troll) Sell(status analysis.MarketStatus) Troll {
   rate := status.Price - 0.5 // Go 50 cents in to ensure trade is snatched up
   usd := troll.Funds.BTC * rate
 
+  usd *= 0.998 // fee
+
   newTrade := btce.OwnTrade{
     Pair: "btc_usd",
     Type: "sell",
@@ -31,6 +33,8 @@ func (troll Troll) Sell(status analysis.MarketStatus) Troll {
 func (troll Troll) Buy(status analysis.MarketStatus) Troll {
   rate := status.Price + 0.5 // Go 50 cents in to ensure trade is snatched up
   btc := troll.Funds.USD / rate
+
+  btc *= 0.998 // fee
 
   newTrade := btce.OwnTrade{
     Pair: "btc_usd",
