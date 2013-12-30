8bda8a70ff2ed814868d88e8fe27d04c3b90d8e7
Artur Sapek <artur@codecademy.com>
2013-12-29 22:55:50 -0500

    Remove file

--- dump.js
+++
@@ -1,3 +0,0 @@
-db.test_prices_analyzed.find().forEach(function (p) {
-  print([p.servertime, p.price, p.analysis.volatility["6"], p.analysis.volatility["12"], p.analysis.volatility["24"]].join(','))
-});
